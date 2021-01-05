ba6cc0858d99a71f7aef82e007b1919d3ebe18d6
Peem <peemtakoonsawat@Peems-MacBook-Pro-2018.local>
2021-01-04 22:25:03 -0800

    ep6

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require("body-parser");
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema }  = require('graphql');
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs')
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const port = process.env.PORT || 5000;
 
@@ -28,11 +30,22 @@ app.use('/graphql', graphqlHTTP({
       date: String!
     }
 
+    type User {
+      _id: ID!,
+      email: String!,
+      password: String
+    }
+    input UserInput {
+      email: String!,
+      password: String!
+    }
+
     type RootQuery {
       events: [Event!]!
     } 
     type RootMutation {
       createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User 
     }      
     schema {
       query: RootQuery
@@ -60,20 +73,57 @@ app.use('/graphql', graphqlHTTP({
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
-        date: new Date( args.eventInput.date )
+        date: new Date( args.eventInput.date ),
+        creator: '5ff402f05e96c9e87afadee2'
       });
+
+      let createdEvent;
 
       // save to mongodb
       return event
         .save()
         .then( data => {
           console.log(data);
-          return { ...data._doc };
+          createdEvent = { ...data._doc };
+          return User.findById('5ff402f05e96c9e87afadee2');
+        })
+        .then( user => {
+          if (!user) {
+            throw new Error('User does not exists');
+          }
+          user.createdEvents.push(event);
+          return user.save();
+        })
+        .then( result => {
+          return createdEvent;
         })
         .catch( err => {
           console.log(err);
           throw err;
         });
+    },
+    createUser: (args) => {
+      return User.findOne({email:args.userInput.email})
+        .then( user => {
+          if (user) {
+            throw new Error('User already exists');
+          }
+          return bcrypt.hash(args.userInput.password, 12);
+        })      
+        .then( hashedPassword => {
+          const user = new User({
+            email: args.userInput.email,
+            password: hashedPassword
+          });
+          return user.save();
+        })
+        .then( user => {
+          return { ...user._doc, password: null };
+        })
+        .catch( err => {
+          throw err;
+        });
+      
     }
 
   },
