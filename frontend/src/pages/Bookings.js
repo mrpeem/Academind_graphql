0c25d05226170bfd9c2f78c28c9b17bb2f1b20d6
Peem <peemtakoonsawat@Peems-MacBook-Pro-2018.local>
2021-01-06 12:11:21 -0800

    ep11

---
+++ frontend/src/pages/Bookings.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class BookingsPage extends Component {
+  render() {
+    return (
+      <h1> bookings page </h1>
+    );
+  }
+}
+
+export default BookingsPage;
