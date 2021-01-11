c9001457b76eced84ed4e84a3ca496f5b99497bb
Peem <mrpeem@gmail.com>
2021-01-11 13:51:51 -0800

    ep19

---
+++ frontend/src/components/Bookings/BookingList/BookingList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './BookingList.css';
+
+const bookingList = props => (
+
+  <ul className="bookings-list">
+    {props.bookings.map(booking => {
+      return (
+        <li key={booking._id} className="bookings-item" key={booking._id}>
+          <div className="bookings-item-data">
+            {booking.event.title} -{' '}
+            {new Date(booking.createdAt).toLocaleDateString()}
+          </div>
+
+          <div className="bookings-item-actions">
+            <button className="btn" 
+              onClick={props.onDelete.bind(this, booking._id)} 
+            > 
+              Cancel 
+            </button>
+          </div>
+         </li>
+      );
+    })}
+  </ul>
+);
+
+export default bookingList;
